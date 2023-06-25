07599d4109aa2664cd2fd573d78d6eada8a1c60d
codergxk <1525185228@qq.com>
2023-06-25 17:45:42 +0800

    feat: extend ImportMetaEnv

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="vite/client" />
 declare module "vue3-json-viewer";
+
+interface ImportMetaEnv {
+  VITE_ENV: string;
+  VITE_APP_NAME: string;
+  VITE_APP_ICO: string;
+  VITE_RecordNo: string;
+  VITE_SERVICE_URL: string;
+  VITE_CaptchaAppId: number;
+  VITE_PRIVACY_AGREEMENT_URL: string;
+  VITE_SERVICE_AGREEMENT_URL: string;
+}
